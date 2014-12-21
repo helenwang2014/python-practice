dd5cd535e217ae3d0d219baf9d4e6d16ee375f15
helenwang2014 <helen.wang2013@gmail.com>
2014-12-21 14:59:05 -0500

    Exercise1

--- Exercise0/test.py
+++ Exercise0/test.py
@@ -2,3 +2,4 @@ print "test"
 
 for i in range(0, 10):
 	print i
+	
---
+++ Exercise1/ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like to type this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "Said" do not touch this.'
+
