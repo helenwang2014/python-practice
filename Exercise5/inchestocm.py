ed3810d67ec2d31aced6c75d86260af6ddc00914
helenwang2014 <helen.wang2013@gmail.com>
2014-12-22 20:15:42 -0500

    Convert inches to centimeters

---
+++ Exercise5/inchestocm.py
@@ -0,0 +1,9 @@
+# Convert inches to centimeters
+
+length = float(raw_input ("Length in inches: "))
+length_in_cm = length * 2.54
+
+# print "Please enter the lenght in inches", length, "then that equals", length_in_cm, "centimeters."
+
+
+print "Please enter the lenght in inches", length, "then that equals %.2f cm." %length_in_cm
