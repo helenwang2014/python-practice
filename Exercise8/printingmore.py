c364849fc3333e87de54ff13e0ae853b8f87c881
helenwang2014 <helen.wang2013@gmail.com>
2014-12-22 22:13:37 -0500

    Add formatter inside formatter

--- Exercise8/printingmore.py
+++ Exercise8/printingmore.py
@@ -1,8 +1,9 @@
-formatter = " %r, %r, %r, %r"
+formatter = " %r %r %r %r"
 
 print formatter % (1, 2, 3, 4)
 print formatter % ('One', 'Two', 'Three', 'Four')
 print formatter % ( True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
 print formatter % (
 	"I had this thing.",
 	"That you could type up right.",
