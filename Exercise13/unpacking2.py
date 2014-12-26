d478f3f85f8cc452a98a1dc58173490e934f635b
helenwang2014 <helen.wang2013@gmail.com>
2014-12-25 19:15:13 -0500

    combine argv and rawinput

---
+++ Exercise13/unpacking2.py
@@ -0,0 +1,8 @@
+from sys import argv
+script, first = argv
+
+print "The script is called: ", script
+print "Your first variable is: ", first
+print "Your second variable is: ", raw_input()
+raw_input("Your third variable is: ")
+
