fce50f4a748d338ea050596eec0117c040272897
helenwang2014 <helen.wang2013@gmail.com>
2014-12-28 17:30:13 -0500

    Functions and Variables

--- Exercise18/function.py
+++ Exercise18/function.py
@@ -1,6 +1,6 @@
 def print_two(*args):
-	arg1, arg2 = args
-	print "arg1: %r, arg2: %r" % (arg1, arg2)
+	print args
+	print "arg1: %r, arg2: %r" % (args[0], args[1])
 
 def print_two_again(arg1, arg2):
 	print "arg1: %r, arg2: %r" % (arg1, arg2)
@@ -11,7 +11,7 @@ def print_one(arg1):
 def print_none():
 	print "I got nothing."
 
-print_two("Zed", "shaw")
+print_two("Zed", "shaw", "1","3",1,2,3,4,5,6,7,8,0)
 print_two_again("Zed", "Shaw")
 print_one("First!")
 print_none()
